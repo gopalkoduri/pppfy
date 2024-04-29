c7f47e9692430a17e3aa2e248f4dfdcf705cee50
Gopala Krishna Koduri <gopala.koduri@gmail.com>
2024-04-29 15:22:14 +0530

    changes to packaging adding readme and other links

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="pppfy",
-    version="1.0.0",
+    version="1.0.1",
     description="A Python package to get prices based on Purchasing Power Parity (PPP)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",  # Ensure you include a README.md in your package
     author="Gopala Krishna Koduri",
     author_email="gopal@riyazapp.com",
-    url="https://riyazapp.com",
+    url="https://github.com/gopalkoduri/pppfy",  # Company URL
+    project_urls={  # Additional URLs
+        "Source Code": "https://github.com/gopalkoduri/pppfy",
+        "Issue Tracker": "https://github.com/gopalkoduri/pppfy/issues",
+        "Connect w/ Author": "https://linkedin.com/in/gopalkoduri",
+        "Riyaz - Learn to sing": "https://riyazapp.com",
+    },
     packages=find_packages(),
     package_data={
         "ppp": ["data/*.csv"],  # Include CSV files in the data directory within the ppp package
